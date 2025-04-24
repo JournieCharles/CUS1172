17af6657937c2716fc55f5f335257a85233abccf
JournieCharles <journie.charles21@my.stjohns.edu>
2025-04-24 17:34:47 -0400

    Update Project2.js

--- Project2.js
+++ Project2.js
@@ -1,5 +1,48 @@
-<script>start("/Users/journiecharles/Project2/");</script>
-<script>onHasParentDirectory();</script>
-<script>addRow("project_02.css","project_02.css",0,324,"324 B",1741318489,"3/6/25, 10:34:49 PM");</script>
-<script>addRow("project_02.html","project_02.html",0,1574,"1.5 kB",1741318489,"3/6/25, 10:34:49 PM");</script>
-<script>addRow("project_02.js","project_02.js",0,1795,"1.8 kB",1741316323,"3/6/25, 9:58:43 PM");</script>
+document.addEventListener("DOMContentLoaded", () => {
+    const taskForm = document.getElementById("task-form");
+    const taskList = document.getElementById("task-list");
+    let tasks = [];
+
+    taskForm.onsubmit = function(event) {
+        event.preventDefault();
+
+        const taskTitle = document.getElementById("task-title").value.trim();
+        const taskPriority = document.getElementById("task-priority").value;
+        const taskStatus = document.querySelector("input[name='task-status']:checked").value;
+
+        if (taskTitle === "") return;
+
+        const task = {title: taskTitle, priority: taskPriority, status: taskStatus, completed: false };
+        tasks.push(task);
+        renderTasks();
+        taskForm.requestFullscreen();
+
+    };
+
+    function renderTasks() {
+        taskList.innerHTML = "";
+        tasks.forEach((task, index) => {
+            const li = document.createElement("li");
+            li.className = "list-group-item d-flex justify-content-between align-items-center";
+
+            if (task.completed) {
+                li.style.textDecoration = "list-through";
+            }
+            li.innerHTML = `
+            ${task.title} - <span class="bage bg-primary">${task.priority}</span> - ${task.status}
+            <div>
+                <button class="btn btn-success btn-sm complete-btn">Mark Complete</button>
+                <button class="btn btn-danger btn-sm remove-btn"> Remove</button>
+            </div>
+            `;
+            li.querySelector(".complete-btn").onclick = () => {
+                task.completed = !task.completed;
+                renderTasks();
+            };
+            li.querySelector(".remove-btn").onclick = () => {
+                tasks.splice(index, 1);
+                renderTasks();
+            }
+        })
+    }
+})
